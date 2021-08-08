d158ceedd37aea7b00b20dfa63138b8b289b4b93
ArnaudB14 <arnaud.barbey14@gmail.com>
2021-08-08 11:46:18 +0000

    add picture

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,7 +1,7 @@
 // == Import npm
 import React from 'react';
 import HomeBackground from 'src/assets/img/home.jpg';
-
+import arnaud from 'src/assets/img/arnaud.png'
 // import CV from 'src/assets/cv.pdf';
 
 // == Import
@@ -17,13 +17,14 @@ const Homepage = () => (
           <h1 className="home__name">Arnaud Barbey</h1>
           <span className="home__profession">Développeur Web Front-end</span>
           <a href="https://www.fichier-pdf.fr/2021/08/08/cv" className="button .button-light">Mon CV</a>
+          <div className="home__photo">
+            <img className="home__card-photo" src={arnaud} />
+          </div>
         </div>
-
         <div className="home__social">
           <a href="https://www.linkedin.com/in/arnaud-barbey/" target="_blank" className="home__social-icon"><i className='bx bxl-linkedin-square'></i></a>
           <a href="https://github.com/ArnaudB14" target="_blank" className="home__social-icon"><i className='bx bxl-github' ></i></a>
         </div>
-
         <div className="home__img">
           <img src={HomeBackground} alt="" />
         </div>
