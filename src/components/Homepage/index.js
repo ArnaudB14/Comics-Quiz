9724c5fd86fd909847bb6b1c39da9aea6c2a40c1
ArnaudB14 <arnaud.barbey14@gmail.com>
2022-05-22 15:33:49 +0000

    change download link

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -14,7 +14,7 @@ const Homepage = () => (
           <span className="home__greeting">Hello, je suis</span>
           <h1 className="home__name">Arnaud Barbey</h1>
           <span className="home__profession">Développeur Web Front-end</span>
-          <a href="../assets/img/CV.pdf" className="button .button-light" download target="_blank">Mon CV</a>
+          <a href="https://www.fichier-pdf.fr/2021/08/08/cv" className="button .button-light">Mon CV</a>
           <div className="home__photo">
             <img className="home__card-photo" src={arnaud} />
           </div>
