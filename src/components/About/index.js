5aebbbceb7f9d1030cb99b6e2635c2622999f9e1
ArnaudB14 <arnaud.barbey14@gmail.com>
2022-05-22 15:28:13 +0000

    change download link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,13 +11,10 @@ import vue from "src/assets/img/vue-svg.svg"
 import react from "src/assets/img/react-svg.svg"
 import redux from "src/assets/img/redux-svg.svg"
 import sass from "src/assets/img/sass-svg.svg"
-import node from "src/assets/img/node-svg.svg"
 import responsive from "src/assets/img/responsive-svg.svg"
 import github from "src/assets/img/github-svg.svg"
 import agile from "src/assets/img/agile-svg.svg"
 import php from "src/assets/img/php-svg.svg"
-import symfony from "src/assets/img/symfony-svg.svg"
-import wordpress from "src/assets/img/wordpress-svg.svg"
 
 // == Import
 import './style.scss';
@@ -53,16 +50,9 @@ const About = () => {
           <div><img src={github} alt="github-svg" /><p>Github</p></div>
           <div><img src={agile} alt="agile-svg" /><p>Agile</p></div>
           <div><img src={php} alt="php-svg" /><p>PHP</p></div>
-        </div>
-
-      <h2 className="skillsToLearn__title About__title">Ce que je souhaite apprendre</h2>
-      <div className="wrapperSkills">
           <div><img src={vue} alt="vue-svg" /><p>Vue.js</p></div>
-          <div><img src={node} alt="node-svg" /><p>Node.js</p></div>
-          <div><img src={symfony} alt="symfony-svg" /><p>Symfony</p></div>
-          <div><img src={wordpress} alt="wordpress-svg" className="wordpress-logo"/><p>Wordpress</p></div>
         </div>
-    </div>
+      </div>
   </div>
 )};
 
