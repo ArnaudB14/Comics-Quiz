6b8c303f0aad9def1ed8ddcbf95be23f37bbf80f
ArnaudB14 <arnaud.barbey14@gmail.com>
2022-10-05 16:55:00 +0200

    nouveau contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,72 +1,82 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
-import Swal from 'sweetalert2';
+import React, { useRef } from 'react';
 import './style.scss';
 
-const SERVICE_ID = 'service_4xolwo9';
-const TEMPLATE_ID = 'template_s5owvzu';
-const USER_ID = 'user_3BmaluZsjevFA0SYKbST5';
+const ContactForm = () => {
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
 
-const Contact = () => {
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
-      (result) => {
-        console.log(result.text);
-        Swal.fire({
-          icon: 'success',
-          title: 'Message envoyé',
-        });
-      },
-      (error) => {
-        console.log(error.text);
-        Swal.fire({
-          icon: 'error',
-          title: 'Ooops, something went wrong',
-          text: error.text,
-        });
-      },
-    );
-    e.target.reset();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      email: emailRef.current.value,
+      message: messageRef.current.value,
+    };
+    console.log(JSON.stringify(data));
   };
 
   return (
-    <div className="contact">
-      <Form onSubmit={handleOnSubmit}>
-        <Form.Field
-          id="form-input-control-email"
-          control={Input}
-          label="Email"
-          name="user_email"
-          placeholder="Email…"
-          required
-          icon="mail"
-          iconPosition="left"
-        />
-        <Form.Field
-          id="form-input-control-last-name"
-          control={Input}
-          label="Nom"
-          name="user_name"
-          placeholder="Nom…"
-          required
-          icon="user circle"
-          iconPosition="left"
-        />
-        <Form.Field
-          id="form-textarea-control-opinion"
-          control={TextArea}
-          label="Message"
-          name="user_message"
-          placeholder="Message…"
-          required
-        />
-        <Button type="submit" color="green">
-          Envoyer
-        </Button>
-      </Form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="background">
+        <div className="container">
+          <div className="screen">
+            <div className="screen-header">
+              <div className="screen-header-left">
+                <div className="screen-header-button close"></div>
+                <div className="screen-header-button maximize"></div>
+                <div className="screen-header-button minimize"></div>
+              </div>
+              <div className="screen-header-right">
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+              </div>
+            </div>
+            <div className="screen-body">
+              <div className="screen-body-item left">
+                <div className="app-title">
+                  <span>CONTACTEZ</span>
+                  <br />
+                  <span>MOI</span>
+                </div>
+                <div className="app-contact">Téléphone : 06 80 71 15 46</div>
+              </div>
+              <div className="screen-body-item">
+                <div className="app-form">
+                  <div className="app-form-group">
+                    <input className="app-form-control" placeholder="NAME" value="ARNAUD BARBEY" />
+                  </div>
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="EMAIL"
+                      type="email"
+                      name="email"
+                      tabIndex="1"
+                      ref={emailRef}
+                    />
+                  </div>
+                  <div className="app-form-group message">
+                    <textarea
+                      className="app-form-control"
+                      placeholder="MESSAGE"
+                      name="message"
+                      tabIndex="2"
+                      ref={messageRef}
+                    />
+                  </div>
+                  <div className="app-form-group buttons">
+                    <button type="submit" className="app-form-button">
+                      ENVOYER
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 };
-export default Contact;
+export default ContactForm;
